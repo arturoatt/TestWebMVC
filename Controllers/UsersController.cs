9b0024cbfaa28d19fb0740553c9848bdf15f93d9
arturoatt <arturoatt@gmail.com>
2021-08-17 22:46:34 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TestWebMVC.Data;
+
+namespace TestWebMVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        private TestWebMVCEntities db = new TestWebMVCEntities();
+
+        // GET: Users
+        public ActionResult Index(string rol)
+        {
+            var roles = db.Role.Select(x => x.Name);
+            ViewBag.Roles = new SelectList(roles);
+
+            if (!String.IsNullOrEmpty(rol))
+            {
+                var aux = db.Role.Include(x => x.User).Where(y => rol.Contains(y.Name)).FirstOrDefault();
+                return View(aux.User);
+            }                
+
+            return View(db.User.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.User.Include(x => x.Role).FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Exp,Alias,Name,Active")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.Id = 1;
+                if (db.User.Count() > 0)
+                    user.Id = db.User.Max(x => x.Id) + 1;
+
+                user.RegistreDate = DateTime.Now;
+                user.UpdateDate = DateTime.Now;
+                db.User.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.User.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Exp,Alias,Name,Active,RegistreDate,UpdateDate")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                //user.UpdateDate = DateTime.Now;
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.User.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.User.Find(id);
+            db.User.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
